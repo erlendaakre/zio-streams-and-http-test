0307c7bd7acce3c30901d8c15ed554c21567764c
Erlend Aakre <erlend@aakre.io>
2021-05-07 10:27:15 +0100

    A hot mess

--- build.sbt
+++ build.sbt
@@ -6,7 +6,6 @@ libraryDependencies += "dev.zio" %% "zio" % "1.0.7"
 libraryDependencies += "dev.zio" %% "zio-streams" % "1.0.7"
 libraryDependencies += "dev.zio" %% "zio-process" % "0.3.0"
 libraryDependencies += "dev.zio" %% "zio-json" % "0.1.4"
-//libraryDependencies += "io.d11" % "zhttp_2.13" % "1.0.0.0-RC15"
 libraryDependencies += "io.d11" % "zhttp" % "1.0.0-SNAPSHOT-RC10"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.7" % "test"
--- src/main/scala/io/aakre/zio_streams_and_http_test/Application.scala
+++ src/main/scala/io/aakre/zio_streams_and_http_test/Application.scala
@@ -10,16 +10,19 @@ import zio.{ExitCode, Ref, URIO, ZIO}
 object Application extends zio.App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = prog.exitCode
 
-  private val state = zio.Runtime.default.unsafeRun(Ref.make(0)) // (╯°□°）╯︵ ┻━┻
+  private val state = zio.Runtime.default.unsafeRun(Ref.make(collection. mutable.ListBuffer.empty[Event])) // (╯°□°）╯︵ ┻━┻
 
   private val blackBox = Command("blackbox.win.exe").linesStream
   private val eventStream = blackBox.map(makeEvent).filter(_.nonEmpty).map(_.get).tap(e => putStrLn(e.toString))
-  private val streamEventsToState = eventStream.foreach(event => state.set(event.timestamp.toInt))
+  private val streamEventsToState = eventStream.foreach(event => for {
+    s <- state.get
+    _ <- state.set(s += event)
+  } yield ())
 
   private val webApp = Http.collectM[Request] {
     case Method.GET -> Root =>
-      state.get.flatMap(s =>
-        ZIO.succeed(Response.text(s"Time: ${System.currentTimeMillis()}\nEvents: $s"))
+      state.get.flatMap(events =>
+        ZIO.succeed(Response.text(superPrettyResult(groupEvents(events.toList).map(s => (s._1, countWords(s._2))))))
       )
   }
 
@@ -37,4 +40,10 @@ object Application extends zio.App {
   def groupEvents(es: List[Event]): Map[EventType, List[Event]] = es.groupBy(_.event_type)
 
   def countWords(es: List[Event]): Map[Word, Int] = es.groupBy(_.data).map(t => (t._1, t._2.size))
+
+  def superPrettyResult(data: Map[EventType, Map[Word,Int]]): String = {
+    def eventTypeToString(eventType: EventType, count: Map[Word,Int]) =
+      s"$eventType\n${"="*eventType.length}\n" + count.mkString("\n")
+    "Event Count\n===========\n\n" + data.map(d => eventTypeToString(d._1, d._2)).mkString("\n\n")
+  }
 }
