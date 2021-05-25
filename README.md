fe10ec424fb45826f171b8171c26d8a2146a66f8
Erlend Aakre <erlend@aakre.io>
2021-05-25 17:06:19 +0100

    windowed request (before) added

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # zio-streams-and-http-test
 
-_A hot mess_
+Simple test of zio-streams and zio-http
+
+## Usage
+- run `Application`
+- browse to  `http://localhost:8090/` to count all events
+- browse to  `http://localhost:8090/after/<unix time>` to count events from that timestamp
+
+## TODO
+- Fix broken use of REF with mutable collection
+- 
--- src/main/scala/io/aakre/zio_streams_and_http_test/Application.scala
+++ src/main/scala/io/aakre/zio_streams_and_http_test/Application.scala
@@ -23,10 +23,15 @@ object Application extends zio.App {
     case Method.GET -> Root =>
       state.get.flatMap(events => ZIO.succeed(Response.jsonString(countWords(events).toJsonPretty)))
 
-    case Method.GET -> Root / "from" / startTimeStr  =>
+    case Method.GET -> Root / "after" / startTimeStr =>
       val startTimeOpt = Try(startTimeStr.toLong).toOption
-      if(startTimeOpt.isEmpty) ZIO.succeed(err)
-      else (state.get.flatMap(events => ZIO.succeed(Response.jsonString(countWords(events, startTimeOpt.get).toJsonPretty))))
+      if (startTimeOpt.isEmpty) ZIO.succeed(err)
+      else state.get.flatMap(events => ZIO.succeed(Response.jsonString(countWords(events, startTimeOpt.get).toJsonPretty)))
+
+    case Method.GET -> Root / "before" / endTimeStr =>
+      val endTimeStrOpt = Try(endTimeStr.toLong).toOption
+      if (endTimeStrOpt.isEmpty) ZIO.succeed(err)
+      else state.get.flatMap(events => ZIO.succeed(Response.jsonString(countWords(events, 0, endTimeStrOpt.get).toJsonPretty)))
   }
 
   private val prog = Server.start(8090, webApp) zipPar streamEventsToState.run
@@ -34,10 +39,10 @@ object Application extends zio.App {
   def makeEvent(s: String): Option[Event] = s.fromJson[Event].toOption
 
   def countWords(events: Iterable[Event], start: Long = 0L, end: Long = Long.MaxValue): Result = Result(
-    events.filter(e => e.timestamp >= start && e.timestamp <= end).
+    events.filter(e => e.timestamp >= start && e.timestamp < end).
       groupBy(_.event_type).map(groupedType => ResultForType(
       groupedType._1, groupedType._2.groupBy(_.data).map(
         groupedWords => (groupedWords._1, groupedWords._2.size)))))
 
-  private val err = Response.fromHttpError(HttpError.BadRequest("Invalid time, times must be valid unix time, eg: \"1621957740\""))
+  private val err = Response.fromHttpError(HttpError.BadRequest("Invalid time, times must be given as unix time, eg: \"1621957740\""))
 }
